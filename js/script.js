a5ff21468cafc4b2702ac0c9a8f2a271630e3b00
toxtaboyevsamandar72 <toxtaboyevsamandar720@gmail.com>
2024-01-14 17:18:48 +0500

    index html Homework

---
+++ js/script.js
@@ -0,0 +1,93 @@
+//  1-masala
+
+
+
+// var berilganSon = -7; 
+
+// if (berilganSon > 0) {
+//     console.log("Berilgan son musbat");
+
+
+// } else {
+//     console.log("Berilgan son manfiy");
+
+
+// }
+
+
+
+//  2-masala
+
+
+
+// var berilganSon = 75;
+// if (berilganSon >= 90 && berilganSon <= 100) {
+//     console.log("A");
+
+
+// }  else if (berilganSon >= 80 && berilganSon <= 89) {
+//     console.log("B");
+
+
+// } else if (berilganSon >= 70 && berilganSon <= 79) {
+//     console.log("C");
+
+
+// }  else if (berilganSon >= 60 && berilganSon <= 69) {
+//     console.log("D");
+
+
+// }  else if (berilganSon >= 0 && berilganSon <= 59) {
+//     console.log("F");
+
+// }
+
+
+//   3-masal  
+
+
+// function aniqlash(kabisaYil) {
+//     if ((kabisaYil % 4 === 0 && kabisaYil % 100 !== 0) || kabisaYil % 400 === 0){
+//       return true;
+//     } else {
+//       return false;
+//   }
+// }
+
+
+// console.log(aniqlash(2024));
+
+
+
+
+// 4 - masala
+
+// let num = 15;
+
+// if (num > 0 ) {
+//     console.log("musbat son");
+// } else if (num < 0) {
+//     console.log("manfiy son");
+// } else if (num == 0) {
+//     console.log("Nol");
+// }
+
+
+
+//  5 - masala
+
+
+
+// let num1 = 20;
+// let num2 = 30;
+
+
+
+// if (num1 > num2 ) {
+//    console.log(num1)
+// } else { 
+// console.log(num2);
+// }
+
+
+
